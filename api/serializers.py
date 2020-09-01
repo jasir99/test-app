470b3cc2d5f198a49bc97fc62c5133a5352ee181
= <=>
2020-09-01 20:33:54 +0200

    Fixed PropertyAddressSerializer class, added User in it.

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from .models import PropertyAddress, PropertyImage
+from user.models import User
 from rest_framework import serializers
 
 class PropertyImageSerializer(serializers.ModelSerializer):
@@ -7,9 +8,17 @@ class PropertyImageSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'phone_number', 'image')
+
+
 class PropertyAddressSerializer(serializers.ModelSerializer):
     images = PropertyImageSerializer(many=True, required=False)
+    user = UserSerializer(required=True)
     class Meta:
         model = PropertyAddress
         fields = ('id', 'city', 'country', 'property_description', 'full_address',
                   'latitude', 'longitude', 'images', 'user')
+
